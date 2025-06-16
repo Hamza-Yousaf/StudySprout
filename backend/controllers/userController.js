06146676309042968d2cfe8e8cafcff23e89fa03
Hamza-Yousaf <hamzayousaf0786@gmail.com>
2025-06-16 22:33:22 +0500

    added password hashing using bcrypt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,17 @@
 import User from '../models/userModel.js';
+import bcrypt from 'bcrypt';
 
 export const createUser = async (req, res) => {
+    const salt = await bcrypt.genSalt(10);
     const user = req.body;
+    const password = user.password;
 
     const newUser = new User(user);
 
     try {
+        const hashedPassword = await bcrypt.hash(password, salt);
+        newUser.password = hashedPassword;
+
         await newUser.save();
         res.status(201).json({ success: true, message: "Successfully created a new user"});
     } catch (error) {
