9a1ccd95544661859f0af4fb5a4a87853421e633
Hamza-Yousaf <hamzayousaf0786@gmail.com>
2025-06-16 22:55:22 +0500

    added user delete request

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import User from '../models/userModel.js';
 import bcrypt from 'bcrypt';
 
@@ -19,3 +20,19 @@ export const createUser = async (req, res) => {
         res.status(500).json({ success: false, message: "(Server Error) in creating a user"});
     }
 };
+
+export const deleteUser = async (req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(404).json({ success: false, message: "User not found"})
+    }
+
+    try {
+        await User.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "User successfully deleted"});
+    } catch (error) {
+        console.error("Error in delete user", error.message);
+        res.status(500).json({ success: false, message: "(Server Error) in deleting a user"});
+    }
+}
