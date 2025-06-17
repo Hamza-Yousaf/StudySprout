e37ecb109ab01b7ae0c650ecacd35fc2b36493e9
Hamza-Yousaf <hamzayousaf0786@gmail.com>
2025-06-17 23:29:07 +0500

    added dark background green color and finished the register page ui

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -4,14 +4,15 @@ const RegisterPage = () => {
   return (
     <div className='relative w-screen h-screen flex items-center justify-center'>
       <div className="absolute top-0 left-0 bg-[var(--backgroundGreen)] w-full h-1/2 z-0"></div>
-      <div className="w-1/3 min-w-[200px] h-4/5 bg-[var(--offWhite)] m-auto shadow-md z-1 p-8">
+      <div className="flex flex-col justify-between w-1/3 min-w-[200px] h-4/5 bg-[var(--offWhite)] m-auto shadow-md z-1 p-8">
         <h1 className='text-2xl text-[var(--darkText)]'>Sign Up to <span className='font-bold'>StudySprout</span></h1>
         <form action="">
-          <input type="text" placeholder='Username' className='border-b w-full mt-8'/>
-          <input type="email" placeholder='Email' className='border-b w-full mt-8'/>
-          <input type="password" placeholder='Password' className='border-b w-full mt-8'/>
-          <input type="submit" className='mt-8 text-center bg-[var(--backgroundGreen)] text-[var(--darkText)] text-xl p-2 shadow-sm font-bold cursor-pointer w-full'/>
+          <input type="text" placeholder='Username' className='border-b w-full mt-8 focus:outline-none focus:text-[var(--darkText)]'/>
+          <input type="email" placeholder='Email' className='border-b w-full mt-8 focus:outline-none focus:text-[var(--darkText)]'/>
+          <input type="password" placeholder='Password' className='border-b w-full mt-8 focus:outline-none'/>
+          <input type="submit" className='mt-8 text-center bg-[var(--backgroundGreen)] text-[var(--darkText)] text-xl p-2 shadow-sm font-bold cursor-pointer w-full hover:bg-[var(--backgroundDarkGreen)]'/>
         </form>
+        <p className='justify-self-end text-[var(--lightText)] text-sm'>Join StudySprout, level up and build your <span className='font-bold'>custom</span> farm with XP gained by staying productive!</p>
       </div>
     </div>
   )
