18bad46a6b99bed09a577d1a53cdcf5572549feb
Hamza-Yousaf <hamzayousaf0786@gmail.com>
2025-06-17 17:28:54 +0500

    register page outline

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <div className='relative w-screen h-screen flex items-center justify-center'>
+      <div className="absolute top-0 left-0 bg-[var(--backgroundGreen)] w-full h-1/2 z-0"></div>
+      <div className="w-1/3 h-4/5 bg-[var(--offWhite)] m-auto shadow-md z-1"></div>
+    </div>
   )
 }
 
