0ff9ed60eddc5e65b357340828caf390ef9b0351
Hamza-Yousaf <hamzayousaf0786@gmail.com>
2025-06-18 15:44:55 +0500

    frontend functionality with email check and toast notification, also added navigation to dashboard upon user creation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
+import { Slide, ToastContainer, Zoom } from 'react-toastify';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <ToastContainer
+      position="bottom-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+      transition={Slide}
+      />
     </BrowserRouter>
   </StrictMode>,
 )
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,17 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const RegisterPage = () => {
+  const navigate = useNavigate();
+  const [newUser, setNewUser] = useState({ 
+    username: "",
+    email: "",
+    password: ""
+  })
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://localhost:5000/api/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body:JSON.stringify(newUser)
+      });
+    
+      if(res.ok) {
+        toast("Account successfully created");
+        navigate("/dashboard");
+      } else {
+        toast("Email is already linked to an account");
+        return;
+      }
+    } catch (error) {
+      console.error(error);
+      toast("Something went wrong, please try again");
+    }
+  }
+
   return (
     <div className='relative w-screen h-screen flex items-center justify-center'>
       <div className="absolute top-0 left-0 bg-[var(--backgroundGreen)] w-full h-1/2 z-0"></div>
       <div className="flex flex-col justify-between w-1/3 min-w-[200px] h-4/5 bg-[var(--offWhite)] m-auto shadow-md z-1 p-8">
         <h1 className='text-2xl text-[var(--darkText)]'>Sign Up to <span className='font-bold'>StudySprout</span></h1>
-        <form action="">
-          <input type="text" placeholder='Username' className='border-b w-full mt-8 focus:outline-none focus:text-[var(--darkText)]'/>
-          <input type="email" placeholder='Email' className='border-b w-full mt-8 focus:outline-none focus:text-[var(--darkText)]'/>
-          <input type="password" placeholder='Password' className='border-b w-full mt-8 focus:outline-none'/>
+
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder='Username' value={newUser.username} className='border-b w-full mt-8 focus:outline-none focus:text-[var(--darkText)]' onChange={(e) => { setNewUser(prev => ({ ...prev, username: e.target.value })) }}/>
+          <input type="email" placeholder='Email' value={newUser.email} className='border-b w-full mt-8 focus:outline-none focus:text-[var(--darkText)]' onChange={(e) => { setNewUser(prev => ({ ...prev, email: e.target.value })) }}/>
+          <input type="password" placeholder='Password' value={newUser.password} className='border-b w-full mt-8 focus:outline-none' onChange={(e) => { setNewUser(prev => ({ ...prev, password: e.target.value })) }}/>
           <input type="submit" className='mt-8 text-center bg-[var(--backgroundGreen)] text-[var(--darkText)] text-xl p-2 shadow-sm font-bold cursor-pointer w-full hover:bg-[var(--backgroundDarkGreen)]'/>
         </form>
+        
         <p className='justify-self-end text-[var(--lightText)] text-sm'>Join StudySprout, level up and build your <span className='font-bold'>custom</span> farm with XP gained by staying productive!</p>
       </div>
     </div>
