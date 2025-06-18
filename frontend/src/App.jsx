bbc5a527d84fcc9f64f4853f37b3f3ebe7d4672f
Hamza-Yousaf <hamzayousaf0786@gmail.com>
2025-06-18 15:48:06 +0500

    created dashboard page and its route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes} from 'react-router'
 import RegisterPage from './pages/RegisterPage'
+import DashboardPage from './pages/DashboardPage'
 
 const App = () => {
   return (
     <div>
        <Routes>
         <Route path="/register" element={<RegisterPage />}/>
+        <Route path="/dashboard" element={<DashboardPage />}/>
        </Routes>
     </div>
   )
