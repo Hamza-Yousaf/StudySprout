72ef17af2d184b6fbb11e110a9117fe1efb8c893
Hamza-Yousaf <hamzayousaf0786@gmail.com>
2025-08-04 19:58:58 -0400

    navigating to login page if trying to load dashboard whilst not logged in

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Logout from "../components/Logout";
 import { useAuthContext } from "../hooks/useAuthContext";
+import { useNavigate } from "react-router";
 
 const DashboardPage = () => {
+  const navigate = useNavigate();
+
   const { user } = useAuthContext();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
+  if (!user) {
+    return null;
+  }
+
   return (
     <div>
-      {user && (
-        <div>
-          <Logout />
-          <span>{user.email}</span>
-        </div>
-      )}
+      <Logout />
+      <span>{user.email}</span>
     </div>
   );
 };
