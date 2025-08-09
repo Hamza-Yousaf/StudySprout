655ed0686f3a5d4397614711cbbc40d0a2f9f04c
Hamza-Yousaf <hamzayousaf0786@gmail.com>
2025-08-09 18:53:29 -0400

    naviagate to /login when not in proper path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,23 @@ import { Route, Routes } from "react-router";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/DashboardPage";
+import { useLocation } from "react-router";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 const App = () => {
+  const location = useLocation();
+  console.log(location);
+  const navigate = useNavigate();
+
+  const paths = ["/register", "/login", "/dashboard"];
+
+  useEffect(() => {
+    if (!paths.includes(location.pathname)) {
+      navigate("/login");
+    }
+  }, [location.pathname, navigate]);
+
   return (
     <div>
       <Routes>
