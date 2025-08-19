b4c200b1320d61772acb5540b18e66124d9c9d12
Hamza-Yousaf <hamzayousaf0786@gmail.com>
2025-08-18 22:30:02 -0400

    basic add course ui

--- frontend/src/pages/CoursesPage.jsx
+++ frontend/src/pages/CoursesPage.jsx
@@ -28,7 +28,41 @@ const CoursesPage = () => {
     <div className="flex bg-[var(--offWhite)]">
       <Sidebar activePath={activePath} username={user.username} />
       <div className="flex flex-col p-4 w-full h-screen ">
-        <h1>Courses</h1>
+        <div className="w-3/4 h-3/4 bg-black shadow-lg rounded-2xl bg-white p-6 m-auto">
+          <h1 className="font-bold text-2xl mb-3">Add a course</h1>
+          <div className="border-b border-gray-300 mb-6"></div>
+
+          <form className="w-1/2 bg-[var(--skyBlue)] shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
+            <div class="mb-4">
+              <label
+                class="block text-black text-xl font-bold mb-2"
+                for="title"
+              >
+                Title
+              </label>
+              <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+                id="title"
+                type="text"
+                placeholder="title..."
+              />
+            </div>
+            <div class="mb-4">
+              <label
+                class="block text-black0 text-xl font-bold mb-2"
+                for="priority"
+              >
+                Priority
+              </label>
+              <select name="priority" id="priority">
+                <option value="Low">Low</option>
+                <option value="Medium">Medium</option>
+                <option value="High">High</option>
+                <option value="Urgent">Urgent</option>
+              </select>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
