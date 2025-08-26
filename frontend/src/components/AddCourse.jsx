82bc131705cb5c8e3e53bee311beb33cf1fbbc68
Hamza-Yousaf <hamzayousaf0786@gmail.com>
2025-08-26 19:55:30 -0400

    componentized and tweaked ui

---
+++ frontend/src/components/AddCourse.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
+
+const AddCourse = () => {
+  const { user } = useAuthContext();
+
+  const handleAddCourse = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    formData.append("userId", user.id);
+
+    const jdata = Object.fromEntries(formData.entries());
+
+    try {
+      const res = await fetch("http://localhost:5000/api/courses", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          //Authorization: `Bearer ${user.token}`,
+        },
+        body: JSON.stringify(jdata),
+      });
+
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {}
+  };
+
+  return (
+    <div className="flex flex-col p-4 w-full  m-auto">
+      <div className="w-1/2 h-1/2 bg-black shadow-lg rounded-2xl bg-white p-6 m-auto ">
+        <h1 className="font-bold text-2xl mb-3">Add a course</h1>
+        <div className="border-b border-gray-300 mb-6"></div>
+
+        <form
+          className="w-full bg-[var(--skyBlue)] shadow-md rounded px-8 pt-6 pb-8 mb-8"
+          onSubmit={handleAddCourse}
+        >
+          <div className="flex justify-between">
+            <div className="mb-4">
+              <label
+                className="block text-black text-xl font-bold mb-2"
+                htmlFor="title"
+              >
+                Title
+              </label>
+              <input
+                name="title"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+                id="title"
+                type="text"
+                placeholder="title..."
+              />
+            </div>
+            <div>
+              <label
+                className="block text-black text-xl font-bold mb-2"
+                htmlFor="deadline"
+              >
+                Exam Date
+              </label>
+              <input
+                name="deadline"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+                id="deadline"
+                type="date"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-black0 text-xl font-bold mb-2"
+                htmlFor="priority"
+              >
+                Priority
+              </label>
+              <select
+                name="priority"
+                id="priority"
+                className="border py-2 px-3 rounded"
+              >
+                <option value="Low">Low</option>
+                <option value="Medium">Medium</option>
+                <option value="High">High</option>
+                <option value="Urgent">Urgent</option>
+              </select>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-[var(--darkText)] py-2 rounded-xl text-white font-semibold hover:bg-[var(--darkBrown)] transition duration-100 ease-in-out transform hover:scale-105"
+          >
+            Add
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddCourse;
--- frontend/src/pages/CoursesPage.jsx
+++ frontend/src/pages/CoursesPage.jsx
@@ -4,6 +4,7 @@ import { useAuthContext } from "../hooks/useAuthContext";
 import { useLocation } from "react-router";
 import Sidebar from "../components/Sidebar";
 import { useEffect } from "react";
+import AddCourse from "../components/AddCourse";
 
 const CoursesPage = () => {
   const navigate = useNavigate();
@@ -12,29 +13,6 @@ const CoursesPage = () => {
   const location = useLocation();
 
   var activePath;
-
-  const handleAddCourse = async (e) => {
-    e.preventDefault();
-
-    const formData = new FormData(e.target);
-    formData.append("userId", user.id);
-
-    const jdata = Object.fromEntries(formData.entries());
-
-    try {
-      const res = await fetch("http://localhost:5000/api/courses", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          //Authorization: `Bearer ${user.token}`,
-        },
-        body: JSON.stringify(jdata),
-      });
-
-      const data = await res.json();
-      console.log(data);
-    } catch (error) {}
-  };
 
   useEffect(() => {
     if (!user) {
@@ -50,77 +28,11 @@ const CoursesPage = () => {
   return (
     <div className="flex bg-[var(--offWhite)]">
       <Sidebar activePath={activePath} username={user.username} />
-      <div className="flex flex-col p-4 w-full  m-auto">
-        <div className="w-1/2 h-1/2 bg-black shadow-lg rounded-2xl bg-white p-6 m-auto">
-          <h1 className="font-bold text-2xl mb-3">Add a course</h1>
-          <div className="border-b border-gray-300 mb-6"></div>
-
-          <form
-            className="w-full bg-[var(--skyBlue)] shadow-md rounded px-8 pt-6 pb-8 mb-8"
-            onSubmit={handleAddCourse}
-          >
-            <div className="flex justify-between">
-              <div className="mb-4">
-                <label
-                  className="block text-black text-xl font-bold mb-2"
-                  htmlFor="title"
-                >
-                  Title
-                </label>
-                <input
-                  name="title"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
-                  id="title"
-                  type="text"
-                  placeholder="title..."
-                />
-              </div>
-              <div>
-                <label
-                  className="block text-black text-xl font-bold mb-2"
-                  htmlFor="deadline"
-                >
-                  Exam Date
-                </label>
-                <input
-                  name="deadline"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
-                  id="deadline"
-                  type="date"
-                />
-              </div>
-              <div className="mb-4">
-                <label
-                  className="block text-black0 text-xl font-bold mb-2"
-                  htmlFor="priority"
-                >
-                  Priority
-                </label>
-                <select
-                  name="priority"
-                  id="priority"
-                  className="border py-2 px-3 rounded"
-                >
-                  <option value="Low">Low</option>
-                  <option value="Medium">Medium</option>
-                  <option value="High">High</option>
-                  <option value="Urgent">Urgent</option>
-                </select>
-              </div>
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-[var(--darkText)] py-2 rounded-xl text-white font-semibold"
-            >
-              Add
-            </button>
-          </form>
-          <div className="flex justify-center">
-            <button className="text-3xl text-[var(--powerBlue)] font-semibold  cursor-pointer underline">
-              View All Courses
-            </button>
-          </div>
-        </div>
+      <div className="flex flex-col w-full h-full m-auto">
+        <AddCourse />
+        <button className="text-3xl text-[var(--powerBlue)] font-semibold cursor-pointer underline">
+          View All Courses
+        </button>
       </div>
     </div>
   );
