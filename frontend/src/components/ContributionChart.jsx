ac9e27fc9b7783ac11a2db155682278b7fe0d159
Hamza-Yousaf <hamzayousaf0786@gmail.com>
2025-09-23 21:56:23 -0400

    contribution chart working per sessions, need to add studyLogs model to store the dates

--- frontend/src/components/ContributionChart.jsx
+++ frontend/src/components/ContributionChart.jsx
@@ -1,25 +1,59 @@
 import React from "react";
 import { ActivityCalendar } from "react-activity-calendar";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useEffect, useState } from "react";
 
-const data = [
-  {
-    date: "2025-01-01",
-    count: 0,
-    level: 0,
-  },
-  {
-    date: "2025-08-13",
-    count: 1,
-    level: 3,
-  },
-  {
-    date: "2025-12-31",
-    count: 0,
-    level: 0,
-  },
+let data = [
+  { date: "2025-01-01", count: 0, level: 0 },
+  { date: "2025-12-31", count: 0, level: 0 },
 ];
 
 const ContributionChart = () => {
+  const { user } = useAuthContext();
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    if (!user.id) {
+      return;
+    }
+
+    const fetchCourses = async () => {
+      try {
+        const res = await fetch(`http://localhost:5000/api/courses/${user.id}`);
+        const response = await res.json();
+        setCourses(response.data);
+      } catch (error) {
+        console.log("error in fetching courses");
+      }
+    };
+
+    fetchCourses();
+  }, [user]);
+
+  useEffect(() => {
+    // check if updatedAt is > todays date at 00:00
+    const today = new Date();
+    const todayFormatted = today.toISOString().slice(0, 10);
+
+    data = [
+      { date: "2025-01-01", count: 0, level: 0 },
+      { date: "2025-12-31", count: 0, level: 0 },
+    ];
+
+    courses.forEach((course) => {
+      const courseDate = new Date(course.updatedAt);
+      const formattedDate = courseDate.toISOString().slice(0, 10);
+      if (formattedDate === todayFormatted && courseDate.getTime() > 0) {
+        console.log("in if");
+        data.push({
+          date: formattedDate,
+          count: 1,
+          level: 1,
+        });
+      }
+    });
+  }, [courses]);
+
   return (
     <div className="w-full h-full bg-black shadow-lg rounded-2xl bg-white p-6 col-start-2 row-start 2 col-span-2">
       <h1 className="font-bold text-2xl mb-3">Tracker</h1>
